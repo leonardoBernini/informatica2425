dd50217feed03b306b1f000eabd734756f7a9069
leonardoBernini <leonardo.bernini@galileiostiglia.edu.it>
2024-09-25 11:45:05 +0000

    compito.inf1.finito

---
+++ informatica/compito.inf1.c
@@ -0,0 +1,21 @@
+/*Calcola il costo sostenuto dalla scolaresca per andare a teatro sapendo che
+il biglietto ordinario costa 12 euro, i 2 docenti accompagnatori hanno lo sconto del 50%
+e che il numero degli alunni deve essere letto in INPUT
+*/
+#include <stdio.h>
+
+float main() {
+    float costobiglietti,nDocenti,nAlunni,costoTotale;
+    costobiglietti=12;
+    nDocenti=2;
+    nAlunni=0,
+    costoTotale=0;
+    
+    printf("inserisci il numero degli alunni ");
+    scanf("%f",&nAlunni);
+   
+    costoTotale=(costobiglietti*nAlunni)+(2*6);
+
+    printf("il costo totale è di: %f",costoTotale);
+    
+}
