a1db80227cc55c62c6604b246ec1fca5634ccdeb
leonardoBernini <leonardo.bernini@galileiostiglia.edu.it>
2024-09-30 19:24:31 +0000

    compiti galotta due

---
+++ informatica/compitidue_31092.c
@@ -0,0 +1,21 @@
+//) Dati 3 numeri stampare il più piccolo, il più grande e la loro media aritmetica
+int main() {
+    int a, b, c;
+    float media;
+
+    
+    printf("Inserisci il primo numero: ");
+    scanf("%d", &a);
+    printf("Inserisci il secondo numero: ");
+    scanf("%d", &b);
+    printf("Inserisci il terzo numero: ");
+    scanf("%d", &c);
+
+
+
+
+
+
+    printf("Il numero più piccolo è: );
+    printf("Il numero più grande è: );
+    printf("La media aritmetica è: );
