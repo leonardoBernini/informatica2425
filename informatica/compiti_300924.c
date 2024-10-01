82f1c89752ff0beb8e3801a5c1ab2d030da546d6
leonardoBernini <leonardo.bernini@galileiostiglia.edu.it>
2024-10-01 07:37:13 +0000

    compito per il 01.10.2024

--- informatica/compiti_300924.c
+++ informatica/compiti_300924.c
@@ -7,12 +7,17 @@ int main(){
     printf("Inserisci un numero: ");
     scanf("%d", &A);
 
-    if (A% 3 == 0 && A% 5 == 0){
-
-    printf("%d è divisibile sia per 3 che per 5", A);
-    } 
-    else{
-    printf("%d non è divisibile sia per 3 che per 5", A);
+    if (A% 3 == 0 ){
+        if(A%5==0){
+            printf("%d è divisibile per 3 e 5, A ");
+        }
+        else{
+            printf("%d è divisibile per 3 ma non per 5, A ");
+        }
+   
     }
-    
+    else{
+        printf(""%d non è divisibile per 3, A ");
+    }
+    return 0;
 }
