b5854a8fc25ab1eae9e4bc3dccce34db422ba6a9
leonardoBernini <leonardo.bernini@galileiostiglia.edu.it>
2024-10-15 11:44:12 +0000

    ok

--- informatica/es.3.numeri.c
+++ informatica/es.3.numeri.c
@@ -6,7 +6,7 @@ considerati in progressione aritmetica; una progressione aritmetica
 #include <stdio.h>
 
 int main() {
-    int num1, num2, num3,diff1,diff2;
+    int num1, num2, num3,dif1,dif2;
     
 
    
@@ -14,11 +14,11 @@ int main() {
     scanf("%d %d %d", &num1, &num2, &num3);
 
     
-    diff1 = num2 - num1;
-    diff2 = num3 - num2;
+    dif1 = num2 - num1;
+    dif2 = num3 - num2;
 
-    if (diff1 == diff2) {
-        printf("I numeri  formano una progressione aritmetica con differenza %d.\n", diff1);
+    if (dif1 == dif2) {
+        printf("I numeri  formano una progressione aritmetica con differenza %d.\n", dif1);
     } else {
         printf("I numeri non formano una progressione aritmetica.");
     }
